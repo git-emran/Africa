8d3702201c1fcdd8c8ee62a0ac9fc9d9bb8d465e
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-09-08 20:54:18 +0600

    Africa app new tab screen

---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Emran Hossain on 6/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
