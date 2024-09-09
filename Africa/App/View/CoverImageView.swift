2a6b9d9bda9d050d7a857d242f4dadb73b222c0c
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-09-09 13:53:37 +0600

    Video and Data model added. New CoverImageView is added

---
+++ Africa/App/View/CoverImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Emran Hossain on 9/9/24.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoverImageView()
+}
